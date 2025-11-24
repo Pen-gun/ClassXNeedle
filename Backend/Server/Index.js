bfc06d341930352e5c37fabd8a19b764f63aec8d
Paras Limbu <paras078@academiacollege.edu.np>
2025-11-24 15:09:10 +0545

    Add /contact route to server
    Introduced a new '/contact' endpoint that responds with a simple message. This enhances the server by providing a contact page route.

--- Backend/Server/Index.js
+++ Backend/Server/Index.js
@@ -13,6 +13,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('This is the about page.')
 })
+app.get('/contact', (req, res) => {
+  res.send('This is the contact page.')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
