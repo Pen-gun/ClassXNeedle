458abc49848b4f3ec665f03b4db0a98a77c4b937
Paras Limbu <paras078@academiacollege.edu.np>
2025-12-22 15:59:29 +0545

    feat: initialize backend with Express, MongoDB connection, and environment configuration
    - Set up Express server with CORS, cookie parsing, and JSON body parsing.
    - Created a database connection helper using Mongoose.
    - Added constants for database configuration.
    - Configured dotenv for environment variable management.
    - Established server listening on specified port after successful database connection.

---
+++ Backend/src/index.js
@@ -0,0 +1,29 @@
+import connectToDB from './db/connectionToDb.helper.js';
+import dotenv from 'dotenv';
+import app from './app.js';
+
+dotenv.config({path: '../.env'});
+
+connectToDB().then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+    app.on('error', (err)=>{
+        console.error("Error starting the server", err);
+    });
+}).catch((err)=>{
+    console.error("Failed to connect to the database", err);
+    process.exit(1);
+});
+
+
+/*;(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log("Connected to the database successfully");
+
+    }catch(err){
+        console.error("Error connecting to the database", err);
+        throw err;
+    }
+})();*/
