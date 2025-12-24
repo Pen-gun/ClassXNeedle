8c521e09bdc4f3ae121b826d443019855f4a8d30
Paras Limbu <paras078@academiacollege.edu.np>
2025-12-24 13:42:33 +0545

    feat: reintroduce models for Category, Order, Product, Review, and User with updated schemas

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express()
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || '*',
+    origin: process.env.CLIENT_URL,
     credentials: true
 }))
 app.use(express.json(
