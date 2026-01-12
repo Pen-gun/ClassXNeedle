9dd18b4f3ee3c2d1d28eb30e0f20d367e84231bd
Paras Limbu <paras078@academiacollege.edu.np>
2026-01-12 14:02:00 +0545

    feat: add smooth scrolling effect on route change in Layout component

--- Frontend/src/components/Layout.tsx
+++ Frontend/src/components/Layout.tsx
@@ -1,8 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [location.pathname]);
 
   return (
     <div className="min-h-screen flex flex-col">
